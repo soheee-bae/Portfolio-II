a9fa2e8538bdf6c78a78b9e93c670206f32c38ca
sohee28 <amybae28@gmail.com>
2023-06-12 21:47:25 -0800

    load model

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import Background from "./components/Background/Background";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Canvas
+      shadows
+      camera={{
+        fov: 60,
+        near: 0.1,
+        far: 400,
+        position: [0, 10, 200],
+      }}>
+      <Background />
+    </Canvas>
+  );
 }
 
 export default App;
