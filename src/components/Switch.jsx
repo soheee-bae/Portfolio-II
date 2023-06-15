7abec5b330bf8a51026f440dc5b21fb302ea8f0a
sohee28 <amybae28@gmail.com>
2023-06-15 00:28:10 -0800

    reposition geometry on scene

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -1,9 +1,17 @@
 import React, { useState, useEffect, useCallback, Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { a } from "@react-spring/three";
-import { useGLTF, useTexture, Shadow, meshBounds } from "@react-three/drei";
+import {
+  useGLTF,
+  useTexture,
+  Shadow,
+  meshBounds,
+  OrbitControls,
+} from "@react-three/drei";
 
 function Switch({ x, set }) {
+  const { nodes, materials } = useGLTF("./model/metal.glb");
+
   const onClick = useCallback(() => set((toggle) => Number(!toggle)), [set]);
 
   const pZ = x.to([0, 1], [-1.2, 1.2]);
@@ -13,6 +21,19 @@ function Switch({ x, set }) {
 
   return (
     <group scale={[1.25, 1.25, 1.25]} dispose={null}>
+      <OrbitControls />
+      <a.mesh
+        receiveShadow
+        castShadow
+        material={materials.track}
+        geometry={nodes.Circle.geometry}
+        material-color={color}
+        material-roughness={0.5}
+        material-metalness={0.8}
+        scale={[1.1, 1.0, 1.2]}
+        rotation={[0, 1.55, 0]}
+        position={[0, 0, 0.05]}
+      />
       <a.group position-y={0.85} position-z={pZ}>
         <a.mesh receiveShadow castShadow rotation-x={rX} onClick={onClick}>
           <sphereGeometry args={[0.8, 64, 64]} />
