d106ce1b12c8571b0d56bc5a0615bdc7876356af
sohee28 <amybae28@gmail.com>
2023-06-16 23:35:57 -0800

    added overlay texts

--- src/components/Overlay/Overlay.jsx
+++ src/components/Overlay/Overlay.jsx
@@ -3,8 +3,19 @@ import "./Overlay.css";
 function Overlay() {
   return (
     <div className="overlay">
-      <p>asdflkjsdfljfd</p>
-      <p>asdflkjsdfljfd</p>
+      <img src="./image/profile.jpg" alt="profile" />
+      <div className="bio">
+        <p>
+          남다른 도전정신을 가진 개발자, <span>배소희</span>입니다.
+        </p>
+        <p>
+          새로운 환경이나 영역에 긍정적으로 도전하는것을 즐기며 <br />
+          꾸준히 성장하는 습관을 만들어 가고 있습니다.
+        </p>
+      </div>
+      <div className="contacts">
+        
+      </div>
     </div>
   );
 }
