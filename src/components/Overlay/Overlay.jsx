032ddf7ef0308b258281fb06e99b1d399a34bc37
sohee28 <amybae28@gmail.com>
2023-06-17 01:18:05 -0800

    added arrow icon

--- src/components/Overlay/Overlay.jsx
+++ src/components/Overlay/Overlay.jsx
@@ -23,11 +23,17 @@ function Overlay({ toggle }) {
 
   return (
     <div className="overlay" style={{ color: `${color}` }}>
-      <img src="./image/profile.jpg" alt="profile" />
+      <div className="imageContainer">
+        <div className="arrows">
+          <img className="arrow1" src="./image/arrow.png" alt="arrow" />
+          <img className="arrow2" src="./image/arrow.png" alt="arrow" />
+        </div>
+        <img className="profile" src="./image/profile.jpg" alt="profile" />
+      </div>
       <div className="bio">
         <p>
           남다른 도전정신을 가진 개발자,{" "}
-          <span style={{ borderBottom: `2px solid ${color}` }}>배소희</span>
+          <span style={{ borderBottom: `3px solid ${color}` }}>배소희</span>
           입니다.
         </p>
         <p>
