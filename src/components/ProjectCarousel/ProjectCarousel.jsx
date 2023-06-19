c14c0021721d282ba4e53cb9f6639542352a4f81
sohee28 <amybae28@gmail.com>
2023-06-18 22:32:42 -0800

    added rough layout for project

--- src/components/ProjectCarousel/ProjectCarousel.jsx
+++ src/components/ProjectCarousel/ProjectCarousel.jsx
@@ -1,19 +1,62 @@
+import { useContext } from 'react';
 import Carousel from 'react-gallery-carousel';
 import 'react-gallery-carousel/dist/index.css';
+
+import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import Tooltip from '@mui/material/Tooltip';
+import LaunchIcon from '@mui/icons-material/Launch';
+
 import styles from './ProjectCarousel.module.scss';
+import ToggleContext from '../../context/toggleContext';
 
 function ProjectCarousel({ project }) {
+  const { isLightMode } = useContext(ToggleContext);
+
+  const color = isLightMode ? 'var(--blackColor200)' : 'var(--whiteColor300)';
+
   return (
-    <Carousel
-      images={project.images}
-      objectFit="contain"
-      hasLeftButton={false}
-      hasRightButton={false}
-      hasMediaButton={false}
-      hasSizeButton={false}
-      hasIndexBoard={false}
-      className={styles.carousel}
-    />
+    <div className={styles.projectCarousel} style={{ color: color }}>
+      <div className={styles.header}>
+        <p className={styles.type}>{project.type}</p>
+        <p className={styles.name}>{project.name}</p>
+      </div>
+      <Carousel
+        images={project.images}
+        objectFit="contain"
+        hasLeftButton={false}
+        hasRightButton={false}
+        hasMediaButton={false}
+        hasSizeButton={false}
+        hasIndexBoard={false}
+        className={styles.carousel}
+      />
+      <p className={styles.description}>{project.description}</p>
+      <ul className={styles.skills}>
+        {project.skills.map((skill) => (
+          <li className={styles.skill} key={skill}>
+            {skill}
+          </li>
+        ))}
+      </ul>
+      <div className={styles.links}>
+        <Tooltip title="Github">
+          <a target="_blank" rel="noreferrer" href={project.github}>
+            <GitHubIcon sx={{ color: color }} />
+          </a>
+        </Tooltip>
+        <Tooltip title="Demo">
+          <a target="_blank" rel="noreferrer" href={project.demo}>
+            <LaunchIcon sx={{ color: color }} />
+          </a>
+        </Tooltip>
+        <Tooltip title="View Detail">
+          <a target="_blank" rel="noreferrer" href={project.demo}>
+            <DescriptionOutlinedIcon sx={{ color: color }} />
+          </a>
+        </Tooltip>
+      </div>
+    </div>
   );
 }
 export default ProjectCarousel;
--- src/components/ProjectStepper/ProjectStepper.jsx
+++ src/components/ProjectStepper/ProjectStepper.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import IconButton from '@mui/material/IconButton';
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
+import ProjectCarousel from '../ProjectCarousel/ProjectCarousel';
 import { projects } from '../../datas/Projects';
 import styles from './ProjectStepper.module.scss';
-import ProjectCarousel from '../ProjectCarousel/ProjectCarousel';
 
 function ProjectStepper() {
   const [activeStep, setActiveStep] = useState(0);
@@ -24,20 +24,25 @@ function ProjectStepper() {
 
   return (
     <div className={styles.projectStepper}>
+      <h1>Project</h1>
       <div className={styles.stepperContent}>
         <IconButton
           onClick={handleBack}
           disabled={activeStep === 0}
           className={styles.backIconButton}
-          sx={{ '&:hover': { backgroundColor: 'transparent' } }}>
-          <ArrowBackIosIcon />
+          sx={{
+            '&:hover': { backgroundColor: 'transparent' }
+          }}>
+          <ArrowBackIosNewIcon />
         </IconButton>
         <ProjectCarousel project={projects[activeStep]} />
         <IconButton
           onClick={handleNext}
           disabled={activeStep === 2}
           className={styles.nextIconButton}
-          sx={{ '&:hover, &:active': { backgroundColor: 'transparent' } }}>
+          sx={{
+            '&:hover': { backgroundColor: 'transparent' }
+          }}>
           <ArrowForwardIosIcon />
         </IconButton>
       </div>
