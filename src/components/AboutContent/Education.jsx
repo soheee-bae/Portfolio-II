6913001304685b0ac9fbe51bd180fa3f7fdf8332
sohee28 <amybae28@gmail.com>
2023-06-26 00:25:01 -0800

    added partial education tab

--- src/components/AboutContent/Education.jsx
+++ src/components/AboutContent/Education.jsx
@@ -1,9 +1,30 @@
 import styles from './AboutContent.module.scss';
+import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
+import EmojiEventsOutlinedIcon from '@mui/icons-material/EmojiEventsOutlined';
+import ComputerOutlinedIcon from '@mui/icons-material/ComputerOutlined';
+import EventNoteOutlinedIcon from '@mui/icons-material/EventNoteOutlined';
 
 function Education() {
   return (
     <div className={styles.content}>
-      <p>Education heasdfsdfasldkfjasdlfkjasdfkj kdjadskjf</p>
+      <div className={styles.educationContent}>
+        <div>
+          <SchoolOutlinedIcon />
+          <p className={styles.subtitle}>University of Alaska Anchorage</p>
+        </div>
+        <div>
+          <ComputerOutlinedIcon />
+          <p>Bachelor of Science, Computer Science </p>{' '}
+        </div>
+        <div>
+          <EventNoteOutlinedIcon />
+          <p>Aug 2015 - Dec 2020</p>
+        </div>
+        <div>
+          <EmojiEventsOutlinedIcon />
+          <p>Honors : cum laude (GPA : 3.66 / 4)</p>
+        </div>
+      </div>
     </div>
   );
 }
