36dcdf7f785708b02e0ccfa662a4deaa746384d7
sohee28 <amybae28@gmail.com>
2023-10-14 17:18:55 +0900

    about page layout

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { a } from '@react-spring/web';
 // import clsx from 'clsx';
 
 // import CursorContext from '../../context/cursorContext';
 import ToggleContext from '../../context/toggleContext';
-// import AboutScene from '../../components/AboutScene/AboutScene';
-// import AboutOverlay from '../../components/AboutOverlay/AboutOverlay';
+import AboutScene from '../../components/AboutScene/AboutScene';
+import AboutOverlay from '../../components/AboutOverlay/AboutOverlay';
 
 import styles from './About.module.scss';
 
@@ -13,8 +13,8 @@ function About() {
   const { x } = useContext(ToggleContext);
   // const { textEnter, textLeave } = useContext(CursorContext);
 
-  // const [aboutSection, setAboutSection] = useState(null);
-  // const [animation, setAnimation] = useState(true);
+  const [aboutSection, setAboutSection] = useState(null);
+  const [animation, setAnimation] = useState(true);
 
   return (
     <a.div
@@ -22,35 +22,31 @@ function About() {
       className={styles.about}
       style={{
         backgroundColor: x.to([0, 1], ['var(--whiteColor400)', 'var(--blackColor300)'])
-      }}>
+      }}
+      // onMouseEnter={() => {
+      //   if (!aboutSection) {
+      //     textEnter('about');
+      //   }
+      // }}
+      // onMouseLeave={() => textLeave()}
+    >
       <h1>ABOUT</h1>
+      <div className={styles.aboutContainer}>
+        <a.div className={styles.aboutContent}>
+          <AboutScene
+            aboutSection={aboutSection}
+            setAboutSection={setAboutSection}
+            setAnimation={setAnimation}
+          />
+        </a.div>
+        <AboutOverlay
+          aboutSection={aboutSection}
+          setAnimation={setAnimation}
+          animation={animation}
+        />
+      </div>
     </a.div>
   );
-
-  // <a.div
-  //   id="about"
-  //   className={styles.about}
-  //   style={{
-  //     backgroundColor: x.to([0, 1], ['var(--whiteColor400)', 'var(--blackColor300)'])
-  //   }}
-  //   onMouseEnter={() => {
-  //     if (!aboutSection) {
-  //       textEnter('about');
-  //     }
-  //   }}
-  //   onMouseLeave={() => textLeave()}>
-  //   <a.div
-  //     className={clsx(styles.aboutContent, {
-  //       [styles.fullWidth]: !aboutSection
-  //     })}>
-  //     <AboutScene
-  //       aboutSection={aboutSection}
-  //       setAboutSection={setAboutSection}
-  //       setAnimation={setAnimation}
-  //     />
-  //   </a.div>
-  //   <AboutOverlay aboutSection={aboutSection} setAnimation={setAnimation} animation={animation} />
-  // </a.div>
 }
 
 export default About;
