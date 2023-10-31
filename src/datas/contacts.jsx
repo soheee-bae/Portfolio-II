182c2cc7eb37d5d1e0d9ab6dbb9b0bd568aab6f5
sohee28 <amybae28@gmail.com>
2023-10-31 15:08:45 +0900

    edited contact page

---
+++ src/datas/contacts.jsx
@@ -0,0 +1,32 @@
+export const contacts =[
+  {
+    label: 'Github',
+    href:'https://github.com/soheee-bae',
+    icon: <></>
+  },
+  {
+    label: 'Email',
+    href:'baesoheee@gmail.com',
+    icon: <></>
+  },
+  {
+    label: 'Linkedin',
+    href:'https://www.linkedin.com/in/sohee-bae-b37a9a166/',
+    icon: <></>
+  },
+  {
+    label: 'Blog',
+    href:'https://soheee-bae.github.io/',
+    icon: <></>
+  },
+  {
+    label: 'Resume',
+    href:'',
+    icon: <></>
+  },
+  {
+    label: 'Portfolio',
+    href:'',
+    icon: <></>
+  },
+]
