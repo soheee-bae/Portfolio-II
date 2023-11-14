74d67be3d6ccf1e889358aa5fc38745652e4dfcd
sohee28 <amybae28@gmail.com>
2023-11-14 19:31:17 +0900

    add accordion component

---
+++ src/components/Accordion/Accordion.jsx
@@ -0,0 +1,37 @@
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import styles from './Accordion.module.scss';
+import clsx from 'clsx';
+
+function AccordionComp(props) {
+  const { title, details, accordion, setAccordion, order } = props;
+
+  console.log(accordion);
+  return (
+    <div className={styles.accordion}>
+      <div className={styles.accordionHeader}>
+        <p className={styles.accordionTitle}>{title}</p>
+        <button
+          className={clsx(styles.expandIcon, {
+            [styles.expandIconClicked]: order === accordion
+          })}
+          onClick={() => {
+            if (order === accordion) {
+              setAccordion(0);
+            } else {
+              setAccordion(order);
+            }
+          }}>
+          <ExpandMoreIcon />
+        </button>
+      </div>
+      <div
+        className={clsx(styles.accordionDetails, {
+          [styles.accordionDetailsExpand]: order === accordion
+        })}>
+        {details}
+      </div>
+    </div>
+  );
+}
+
+export default AccordionComp;
