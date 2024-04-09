0ca82b8c66f8dcd56e2401a6c5c3a463831192ef
SoHee <amybae28@gmail.com>
2024-04-09 21:26:47 +0900

    clean

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -2,8 +2,6 @@ import styles from './Skills.module.scss';
 import Layout from '../../components/Layout/Layout';
 import { useContext } from 'react';
 import ToggleContext from '../../context/toggleContext';
-import { LightBulb, LeftCurveArrow } from '../../icons';
-import clsx from 'clsx';
 
 function Skills() {
   const { isLightMode } = useContext(ToggleContext);
@@ -11,24 +9,12 @@ function Skills() {
   //   const [display, setDisplay] = useState(false);
 
   const color = isLightMode ? 'var(--blackColor200)' : 'var(--whiteColor300)';
+  console.log(color);
   //   const arrowSrc = isLightMode ? './image/curly-arrow.png' : './image/curly-arrowWhite.png';
 
   return (
     <Layout className={styles.skillsContainer}>
-      <div className={styles.skillsContent}>
-        <div className={clsx(styles.skillsSubtitle, styles.firstSubtitle)}>
-          <p>Frontend</p>
-          <LeftCurveArrow color={color} />
-        </div>
-        <div className={styles.skillsTitle}>
-          <LightBulb color={color} />
-          <p>Skills</p>
-        </div>
-        <div className={clsx(styles.skillsSubtitle, styles.secondSubtitle)}>
-          <p>Tools & Collaboration</p>
-          <LeftCurveArrow color={color} />
-        </div>
-      </div>
+      <div className={styles.skillsContent}></div>
     </Layout>
   );
 }
